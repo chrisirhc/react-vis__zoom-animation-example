a14ab8c31939d6032991f3d62b70d99609d2a7cf
Chris Chua <chris.sirhc@gmail.com>
2016-04-28 23:23:30 -0700

    initial zoom working

--- example.js
+++ example.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Motion, spring} from 'react-motion'
 
 import {
   XYPlot,
@@ -17,6 +18,11 @@ export default class Example extends React.Component {
     };
     this._rememberValue = this._rememberValue.bind(this);
     this._forgetValue = this._forgetValue.bind(this);
+    this._zoom = this._zoom.bind(this);
+  }
+
+  _zoom() {
+    this.setState({zoomed: !this.state.zoomed});
   }
 
   _rememberValue(value) {
@@ -30,28 +36,41 @@ export default class Example extends React.Component {
   }
 
   render() {
-    const {value} = this.state;
+    const {value, zoomed} = this.state;
     return (
-      <XYPlot
-        width={300}
-        height={300}>
-        <VerticalGridLines />
-        <HorizontalGridLines />
-        <XAxis />
-        <YAxis />
-        <MarkSeries
-          onValueMouseOver={this._rememberValue}
-          onValueMouseOut={this._forgetValue}
-          data={[
-            {x: 1, y: 10},
-            {x: 2, y: 5},
-            {x: 3, y: 15}
-          ]}/>
-        {value ?
-          <Hint value={value}/> :
-          null
+      <div>
+        <button onClick={this._zoom}>Zoom</button>
+        <Motion style={{
+          left: spring(zoomed ? 1 : 1),
+          right: spring(zoomed ? 2 : 3)
+        }}>
+        {
+          ({left, right}) =>
+          <XYPlot
+            width={300}
+            height={300}
+            xDomain={[left, right]}
+          >
+            <VerticalGridLines />
+            <HorizontalGridLines />
+            <XAxis />
+            <YAxis />
+            <MarkSeries
+              onValueMouseOver={this._rememberValue}
+              onValueMouseOut={this._forgetValue}
+              data={[
+                {x: 1, y: 10},
+                {x: 2, y: 5},
+                {x: 3, y: 15}
+              ]}/>
+            {value ?
+              <Hint value={value}/> :
+              null
+            }
+          </XYPlot>
         }
-      </XYPlot>
+        </Motion>
+      </div>
     );
   }
 }
