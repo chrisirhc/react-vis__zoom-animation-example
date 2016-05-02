06571aae4a3d05a872e66e1714b4221ef550bf73
Chris Chua <chris.sirhc@gmail.com>
2016-05-01 23:37:28 -0700

    clean up examples, add other animation types

--- example.js
+++ example.js
@@ -10,66 +10,120 @@ import {
   MarkSeries,
   Hint} from 'react-vis';
 
+function NoAnimationExample({isZoomed}) {
+  const left = isZoomed ? 1: 1;
+  const right = isZoomed ? 2: 3;
+  return (
+    <XYPlot
+      animation={false}
+      width={300}
+      height={300}
+      xDomain={[left, right]}
+    >
+      <VerticalGridLines />
+      <HorizontalGridLines />
+      <XAxis />
+      <YAxis />
+      <MarkSeries
+        data={[
+          {x: 1, y: 10},
+          {x: 2, y: 5},
+          {x: 3, y: 15}
+        ]}/>
+    </XYPlot>
+  );
+}
+
+function ReactVisAnimationExample({isZoomed}) {
+  const left = isZoomed ? 1: 1;
+  const right = isZoomed ? 2: 3;
+  return (
+    <XYPlot
+      animation={{duration: 400}}
+      width={300}
+      height={300}
+      xDomain={[left, right]}
+    >
+      <VerticalGridLines />
+      <HorizontalGridLines />
+      <XAxis />
+      <YAxis />
+      <MarkSeries
+        data={[
+          {x: 1, y: 10},
+          {x: 2, y: 5},
+          {x: 3, y: 15}
+        ]}/>
+    </XYPlot>
+  );
+}
+
+function ReactMotionAnimationExample({isZoomed}) {
+  return (
+    <Motion style={{
+      left: spring(isZoomed ? 1 : 1),
+      right: spring(isZoomed ? 2 : 3)
+    }}>
+    {
+      ({left, right}) =>
+      <XYPlot
+        animation={false}
+        width={300}
+        height={300}
+        xDomain={[left, right]}
+      >
+        <VerticalGridLines />
+        <HorizontalGridLines />
+        <XAxis />
+        <YAxis />
+        <MarkSeries
+          data={[
+            {x: 1, y: 10},
+            {x: 2, y: 5},
+            {x: 3, y: 15}
+          ]}/>
+      </XYPlot>
+    }
+    </Motion>
+  );
+}
+
+const optionToComponent = {
+  'No Animation': NoAnimationExample,
+  'react-vis Animation': ReactVisAnimationExample,
+  'react-motion Animation': ReactMotionAnimationExample
+};
+
 export default class Example extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: null
+      selectedOption: 'react-motion Animation'
     };
-    this._rememberValue = this._rememberValue.bind(this);
-    this._forgetValue = this._forgetValue.bind(this);
+    this._onChange = this._onChange.bind(this);
     this._zoom = this._zoom.bind(this);
+  }
+
+  _onChange({target: {value: selectedOption}}) {
+    this.setState({selectedOption});
   }
 
   _zoom() {
     this.setState({zoomed: !this.state.zoomed});
   }
 
-  _rememberValue(value) {
-    this.setState({value});
-  }
-
-  _forgetValue() {
-    this.setState({
-      value: null
-    });
-  }
-
   render() {
-    const {value, zoomed} = this.state;
+    const {selectedOption, zoomed} = this.state;
+    const Component = optionToComponent[selectedOption];
     return (
       <div>
+        <select onChange={this._onChange} value={selectedOption}>
+          {Object.keys(optionToComponent)
+            .map(k => <option key={k} value={k}>{k}</option>)}
+        </select>
+
         <button onClick={this._zoom}>Zoom</button>
-        <Motion style={{
-          left: spring(zoomed ? 1 : 1),
-          right: spring(zoomed ? 2 : 3)
-        }}>
-        {
-          ({left, right}) =>
-          <XYPlot
-            width={300}
-            height={300}
-            xDomain={[left, right]}
-          >
-            <VerticalGridLines />
-            <HorizontalGridLines />
-            <XAxis />
-            <YAxis />
-            <MarkSeries
-              onValueMouseOver={this._rememberValue}
-              onValueMouseOut={this._forgetValue}
-              data={[
-                {x: 1, y: 10},
-                {x: 2, y: 5},
-                {x: 3, y: 15}
-              ]}/>
-            {value ?
-              <Hint value={value}/> :
-              null
-            }
-          </XYPlot>
-        }
-        </Motion>
+        <Component isZoomed={zoomed} />
       </div>
     );
   }
